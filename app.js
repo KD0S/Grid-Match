0262d4796ebf21e872d44d614845a0d1862528c8
Krishnadas Nair <76253933+KrishnadasNair-dev@users.noreply.github.com>
2021-06-13 20:40:59 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ let temp;
 let blankPosition;
 let timer = document.querySelector('.timer');
 let moves = document.querySelector('.moves');
-var resetbutton = document.querySelector('#reset');
-
+let resetButton = document.querySelector('.reset');
 let grids = document.querySelectorAll('.grid-item');
 var obj = document.querySelectorAll('.obj-item');
-
+let winarray = []
+let count = 0
+let move = 0
+let time = 0
 
 
 let gridid = [["1","2","3","4","5"],
@@ -21,6 +23,8 @@ let gridid = [["1","2","3","4","5"],
 ["21","22","23","24","25"]
 ]
 
+let winid = ["7","8","9","12","13","14","17","18","19"]
+
 
 function swap(a,b){
     temp=a
@@ -28,9 +32,12 @@ function swap(a,b){
     b=temp
 }
 
+
+
 function blankpos(){
  for(k=0;k<grids.length;k++){
     if(grids[k].style.backgroundColor=="black"){
+      blankPosition=grids[k]
       for(i=0;i<5;i++){
          for(j=0;j<5;j++){
              if(grids[k].id==gridid[i][j]){
@@ -45,17 +52,17 @@ function blankpos(){
 function blankgrid() {
     blankPosition = grids[Math.floor(Math.random()*grids.length)]
     blankPosition.style.backgroundColor = "black"
-    
     blankpos()
 }
 function randomsquare(){
  for (i = 0; i < grids.length; i++) {
+    var colour = colors[Math.floor(Math.random()*colors.length)]
+    if(i<obj.length){
+        obj[i].style.backgroundColor= colour
+    }
      if(grids[i]!==blankPosition){
-        var colour = colors[Math.floor(Math.random()*colors.length)]
         grids[i].style.backgroundColor = colour
-        if(i<obj.length){
-            obj[i].style.backgroundColor= colour
-        }
+        
     }
   }
 }
@@ -63,38 +70,48 @@ function randomsquare(){
 blankgrid()
 randomsquare()
 
+
+var myVar;
+
+function myFunction() {
+  myVar = setInterval(timerfun, 1000);
+}
+myFunction()
+
+function timerfun() {
+  time++;
+  timer.innerHTML = time
+}
+
+
+
+
 for(i=0;i<grids.length;i++){
     grids[i].addEventListener("click",gridswap)
 }
 
+
+
 function gridswap(){
-    
     gridpos(this.id)
     if(Math.abs(gridi-blanki)==1&&Math.abs(gridj-blankj)==0){
       blankPosition.style.backgroundColor = this.style.backgroundColor
       this.style.backgroundColor="black"
-      temp = gridid[gridi][gridj]
-      gridid[gridi][gridj] = gridid[blanki][blankj]
-      gridid[blanki][blankj] = temp
       blankpos()
-      
+      move++
+      moves.innerHTML=move
     }
-    
     else if(Math.abs(gridi-blanki)==0&&Math.abs(gridj-blankj)==1){
         blankPosition.style.backgroundColor = this.style.backgroundColor
         this.style.backgroundColor="black"
-        temp = gridid[gridi][gridj]
-        gridid[gridi][gridj] = gridid[blanki][blankj]
-        gridid[blanki][blankj] = temp
         blankpos()
-        }
-    
-    grids = document.querySelectorAll('.grid-item');
+        move++
+        moves.innerHTML=move
+    }
+    win()
     
 }     
         
-
-
 function gridpos(x){
      for(i=0;i<5;i++){
          for(j=0;j<5;j++){
@@ -107,11 +124,45 @@ function gridpos(x){
 }
 
 
-function reset(){
+
+function win(){
+    count=0
+    for(i=0;i<winid.length;i++){
+        for(j=0;j<grids.length;j++){
+              if(winid[i]==grids[j].id){
+                  winarray[i]=grids[j]
+              }
+                
+         } 
+     }
+     for(i=0;i<obj.length;i++){
+         if(obj[i].style.backgroundColor==winarray[i].style.backgroundColor){
+             count+=1
+             if(count==9){
+                 count = 0
+                 alert("Congratulation! You've Won!")
+                 reset()
+             }
+         }
+         else{
+             count = 0
+         }
+      }
+      
+ }
+
+resetButton.addEventListener("click", function(){
+	reset();
+    console.log("reset")
+})
+
+ function reset(){
 	blankgrid()
     randomsquare()
+    moves.innerHTML=0
+    timer.innerHTML=0
+    move = 0
+    count = 0
+    time = 0
+   
 }
-
-resetbutton.addEventListener("click", function(){
-	reset();
-})
